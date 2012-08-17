2e8bd12183339fc38fd586ba112e1a3306b6dcd4
Kevin Neaton <neatonk@gmail.com>
2012-08-17 08:33:18 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# shah
+
+Secure one-way hashing
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2012 Kevin Neaton
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject shah "0.1.0-SNAPSHOT"
+  :description "Secure one-way hashing"
+  :url "http://github.com/neatonk/shah"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
